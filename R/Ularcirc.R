b1e65a7eb937dcc69a3d0a7b0e07c9877b0bf47d
David Humphreys <d.humphreys@victorchang.edu.au>
2018-04-01 21:12:46 +1000

    Dont test flag set for Ularcirc example.

--- R/Ularcirc.R
+++ R/Ularcirc.R
@@ -7,8 +7,10 @@
 #' @examples
 #' # The following commands will load the shiny app either through an RStudio session or
 #' # through your internet browser
+#' \donttest{
 #' library("Ularcirc")
 #' Ularcirc()
+#' }
 #' @export
 Ularcirc <- function()
 {
